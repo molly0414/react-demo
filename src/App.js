23fd2b7fdd2de4ce6abe89f40924ed5ff88ef782
Molly_Wu <760414@gmail.com>
2021-11-16 23:22:42 +0800

    update header and menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Input } from 'antd';
 import { HomeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import 'App.css';
 import MyCart from "pages/MyCart";
@@ -9,16 +9,29 @@ import ProductDetail from 'pages/ProductDetail';
 import ProductsProvider from "contexts/ProductsProvider";
 
 const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
 function App() {
   const [current, setCurrent] = useState('home');
-
+  const onSearch = (value) => console.log(value);
   return (
     <ProductsProvider>
       <Router>
         <Layout className="layout">
-          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-            <Menu theme="dark" onClick={(e) => setCurrent(e.key)} selectedKeys={[current]} mode="horizontal">
+          <Header className="header">
+            <div className="logo"><Link to="/react-demo">Test Logo</Link></div>
+            <Search
+              className="search hidden-sm"
+              placeholder="input search text"
+              allowClear
+              onSearch={onSearch}
+            />
+            <Menu 
+              className="menu" 
+              theme="light" 
+              onClick={(e) => setCurrent(e.key)} 
+              selectedKeys={[current]} 
+              mode="horizontal">
               <Menu.Item key="home" icon={<HomeOutlined />}><Link to="/react-demo">Home</Link></Menu.Item>
               <Menu.Item key="cart" icon={<ShoppingCartOutlined />}><Link to="/my-cart">My Cart</Link></Menu.Item>
             </Menu>
